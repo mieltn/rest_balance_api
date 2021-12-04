d826e3ea453a9be6490d00bbbebd59fdb7d421ca
mieltn <mike.elyutin@gmail.com>
2021-12-04 12:14:40 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,192 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+# initialize app, reference database and configure database connection
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Client(db.Model):
+    '''
+    Table model to store clients.
+    Clients have ids and balances.
+    '''
+
+    __tablename__ = 'clients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    balance = db.Column(db.Float)
+
+    def serialize(self):
+        return {
+            'client_id': self.id,
+            'balance': self.balance,
+        }
+
+
+class Transaction(db.Model):
+    '''
+    Table model to store transactions.
+    Transactions have ids, amounts and seller/buyer keys refrencing to clients table.
+    Clients reference is useful to update balances, when transfer is requested.
+    '''
+
+    __tablename__ = 'transactions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float)
+    seller_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
+    buyer_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
+
+    seller = db.relationship('Client', foreign_keys=[seller_id])
+    buyer = db.relationship('Client', foreign_keys=[buyer_id])
+
+
+    def serialize(self):
+        return {
+            'transaction_id': self.id,
+            'amount': self.amount,
+            'seller_id': self.seller_id,
+            'buyer_id': self.buyer_id
+        }
+
+
+@app.get('/api/client/balance/<int:client_id>')
+def getBalance(client_id):
+    '''
+    Gets balance of a particular user.
+    Client id should be sent in get request url.
+    Returns status and client instance.
+    '''
+
+    client = Client.query.get_or_404(client_id)
+    return jsonify(
+        status=200,
+        output=client.serialize()
+    )
+
+
+@app.post('/api/client/add')
+def addClient():
+    '''
+    Adds new client to database.
+    Uses post method and json argument to provide balance for new client.
+    Returns status and added client instance.
+    '''
+
+    if request.is_json:
+        new_client = Client(
+            balance = request.json['balance']
+        )
+            
+        db.session.add(new_client)
+        db.session.commit()
+        return jsonify(
+            status=201,
+            added=new_client.serialize()
+        )
+
+    return jsonify(
+        status=400,
+        message='Pass valid json argument to the request header'
+    )
+
+
+@app.patch('/api/client/update/<int:client_id>')
+def updateBalance(client_id):
+    '''
+    Increases or decreases balance of a given client.
+    Client id should be sent in request url.
+    Action and amount should be provided with json argument in patch request.
+    '''
+
+    if request.is_json:
+        client = Client.query.get_or_404(client_id)
+        amount = request.json['amount']
+
+        # if action = credit, we need to increase balance
+        # else before action we check if amount is less or equal to available balance
+        if request.json['action'] == 'credit':
+            client.balance += amount
+        else:
+            if amount > client.balance:
+                return jsonify(
+                    status=501, 
+                    message=f'Failed to update balance. Not enough money on buyer\'s account',
+                    client=client.serialize()
+                )
+            client.balance -= amount
+        db.session.commit()
+
+        return jsonify(
+            status=200,
+            updated=client.serialize()
+        )
+
+    return jsonify(
+        status=400,
+        message='Pass valid json argument to the request header'
+    )
+
+
+@app.post('/api/transaction')
+def addTransaction():
+    '''
+    Performs new transaction.
+    Checks if buyer and seller are presented in clients table.
+    If not, adds them with 0 balances.
+    Checks balance of buyer, to be sure that the requsted amount could be provided.
+    Updates balances of buyer and seller.
+    Adds transaction to database.
+    '''
+
+    if request.is_json:
+
+        seller_id = request.json['seller_id']
+        buyer_id = request.json['buyer_id']
+        # variable to check if provided clients exist
+        seller_exists = Client.query.get(seller_id)
+        buyer_exists = Client.query.get(buyer_id)
+
+        # if any of clients absent, call client add method
+        add_url = 'http://127.0.0.1:5000/api/client/add'
+        params = {'balance': 0}
+        if not seller_exists:
+            seller_resp = requests.post(add_url, json=params).json()
+            seller_id = seller_resp['added']['client_id']
+
+        if not buyer_exists:
+            buyer_resp = requests.post(add_url, json=params).json()
+            buyer_id = buyer_resp['added']['client_id']
+
+        # update balamces and add transaction
+        update_url = 'http://127.0.0.1:5000/api/client/update/'
+        amount = request.json['amount']
+
+        # first, try to call update on buyer to make sure there are enougth money
+        params_buyer = {'amount': amount, 'action': 'writeoff'}
+        update_buyer_resp = requests.patch(update_url + str(buyer_id), json=params_buyer).json()
+        # if request was executed successfully, update seller balance
+        # then add new transaction to database
+        if update_buyer_resp['status'] == 200:
+            params_seller = {'amount': amount, 'action': 'credit'}
+            update_seller_resp = requests.patch(update_url + str(seller_id), json=params_seller).json()
+            
+            new_transaction = Transaction(
+                amount = amount,
+                seller_id = seller_id,
+                buyer_id = buyer_id,
+            )
+
+            db.session.add(new_transaction)
+            db.session.commit()
+
+            return jsonify(
+                status=201,
+                added=new_transaction.serialize()
+            )
+
+        return update_buyer_resp
+        
---
+++ testing.py
@@ -0,0 +1,80 @@
+import requests
+
+def populate(balances):
+    '''
+    Populate database with clients for testing
+    '''
+
+    url = 'http://127.0.0.1:5000/api/client/add'
+    for balance in balances:
+        params = {'balance': balance}
+        r = requests.post(url, json=params)
+    return 'ok'
+
+
+def get_balance(client_id):
+    '''
+    Test function for getting balance of a given client.
+    '''
+
+    url = 'http://127.0.0.1:5000/api/client/balance/'
+    r = requests.get(url + str(client_id)).json()
+    return r
+
+
+def update_balance(client_id, amount, action):
+    '''
+    Test function for updating balance for a given amount.
+    Depending on the action received, balance could be increased or decreased.
+    '''
+
+    url = 'http://127.0.0.1:5000/api/client/update/'
+    params = {'amount': amount, 'action': action}
+    r = requests.patch(url + str(client_id), json=params).json()
+    return r
+
+
+def transfer(seller_id, buyer_id, amount):
+    '''
+    Test function for performing new transaction.
+    '''
+
+    url = 'http://127.0.0.1:5000/api/transaction'
+    params = {'amount': amount, 'seller_id': seller_id, 'buyer_id': buyer_id}
+    r = requests.post(url, json=params).json()
+    return r
+
+
+if __name__ == '__main__':
+
+    # populate db with 5 clients and given balances
+    balances = [1, 55, 500, 200, 12.34]
+    print(populate(balances))
+
+    # get balance of the third client
+    # correct answer: 500
+    print(get_balance(3))
+
+    # update balance of the third client
+    # first tests credit action for increasing balance
+    # second tests writeoff action for decreasing balance
+    # after both actions performed the correct balance: 499
+    print(update_balance(3, 100, 'credit'))
+    print(update_balance(3, 101, 'writeoff'))
+
+    # add new transaction
+    # transfer from 4th client to 1st in amount of 20
+    # get the balances of clients
+    # correct anser: 1 - 21, 4 - 180
+    print(transfer(1, 4, 20))
+    print(get_balance(1))
+    print(get_balance(4))
+
+
+    # testing error cases
+    
+
+
+
+
+
